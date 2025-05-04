31e9f5b5ab67031ca7f74c497c4f0bc19ed24792
dayani04 <144075842+dayani04@users.noreply.github.com>
2025-05-04 17:36:39 +0530

    Update villagerRoutes.js
    fixed villager error

--- backend/src/routes/villager/villagerRoutes.js
+++ backend/src/routes/villager/villagerRoutes.js
@@ -1,7 +1,5 @@
-//villagerRoutes.js
 const express = require('express');
 const villagerController = require('../../controllers/villager/villagerController');
-const villageOfficerController = require ('../../controllers/villageOfficer/villageOfficerController')
 const authenticate = require('../../middleware/authMiddleware');
 const router = express.Router();
 
@@ -16,12 +14,7 @@ router.get("/:id", authenticate, villagerController.getVillager);
 router.put("/:id", authenticate, villagerController.updateVillager);
 router.delete("/:id", authenticate, villagerController.deleteVillager);
 router.put("/:id/status", authenticate, villagerController.updateUserStatus);
-
 router.post("/:id/request-otp", authenticate, villagerController.requestPasswordOtp);
 router.post("/:id/verify-otp", authenticate, villagerController.verifyPasswordOtp);
-router.put("/:id/password", authenticate, villagerController.updateUserPassword);
-router.get("/profile", authenticate, villagerController.getProfile);
-router.get("/",authenticate, villageOfficerController.getAllVillagers)
-
 
 module.exports = router;
